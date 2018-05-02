a7d7329b1f2e409c3d0e161baeab626f7ef794c5
Alex <griswoldalex41@gmail.com>
2018-05-02 12:00:34 -0400

    Link compile sources and add bridging header.

---
+++ MPShare-Bridging-Header.h
@@ -0,0 +1,19 @@
+//
+//  MPShare-Bridging-Header.h
+//  MPShare
+//
+//  Created by Alexander Griswold on 5/2/18.
+//  Copyright © 2018 com.Mobilepic. All rights reserved.
+//
+
+#ifndef MPShare_Bridging_Header_h
+#define MPShare_Bridging_Header_h
+
+#import "MPShare/peertalk/Peertalk.h"
+#import "MPShare/peertalk/PTChannel.h"
+#import "MPShare/peertalk/PTPrivate.h"
+#import "MPShare/peertalk/PTProtocol.h"
+#import "MPShare/peertalk/PTUSBHub.h"
+#import "MPShare/peertalk/PTProtocol.h"
+
+#endif /* MPShare_Bridging_Header_h */
